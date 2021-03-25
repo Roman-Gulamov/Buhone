67f4bed6de5695aa37b9973ade8d2f8d79305728
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-03-26 00:18:37 +0300

    Added data

---
+++ src/Components/Header/NavMap.js
@@ -0,0 +1,26 @@
+export const NAV_LINKS = [
+    {
+        path: 'main',
+        title: 'главная'
+    },
+    {
+        path: 'services',
+        title: 'услуги'
+    },
+    {
+        path: 'about',
+        title: 'кейсы'
+    },
+    {
+        path: 'clients',
+        title: 'наши клиенты'
+    },
+    {
+        path: 'reviews',
+        title: 'отзывы'
+    },
+    {
+        path: 'contact',
+        title: 'контакты'
+    }
+]
