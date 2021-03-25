2a11f621392a8ce34ae59f085b40c2abd14e5f59
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-03-26 00:15:36 +0300

    Updated imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from './Components/App';
 
-import App from './Components/App';
+import './assets/fonts/lato/stylesheet.css';
+import './assets/fonts/merriweather/stylesheet.css';
+
 
 ReactDOM.render(<App />,document.getElementById('root'));
