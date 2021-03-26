6bc5067c50892b9aaf3ad212ab3699745f60b68a
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-03-27 02:19:01 +0300

    Added data

---
+++ src/Components/Header/HeaderData.js
@@ -0,0 +1,58 @@
+import { faMobileAlt, faClock, faEnvelope, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+
+
+export const INFORM_DATA = [
+    {
+        id: '1',
+        avatar: faMobileAlt,
+        title: '8 (111) 222-33-44'
+    },
+    {
+        id: '2',
+        avatar: faClock,
+        title: 'Пн-Пт 10:00-18:00'
+    },
+    {
+        id: '3',
+        avatar: faEnvelope,
+        title: 'order@buhone.ru'
+    },
+    {
+        id: '4',
+        avatar: faMapMarkerAlt,
+        title: 'Невский пр. 130'
+    }
+]
+
+export const NAV_DATA = [
+    {
+        id: '1',
+        path: 'main',
+        title: 'главная'
+    },
+    {
+        id: '2',
+        path: 'services',
+        title: 'услуги'
+    },
+    {
+        id: '3',
+        path: 'about',
+        title: 'кейсы'
+    },
+    {
+        id: '4',
+        path: 'clients',
+        title: 'наши клиенты'
+    },
+    {
+        id: '5',
+        path: 'reviews',
+        title: 'отзывы'
+    },
+    {
+        id: '6',
+        path: 'contact',
+        title: 'контакты'
+    }
+]
