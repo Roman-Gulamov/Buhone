530fa54063ce5c7d820e4df72a4640d7e922f70a
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-03-27 02:17:18 +0300

    Correcting paddings

--- src/styles/Container.js
+++ src/styles/Container.js
@@ -1,8 +1,15 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    padding-right: 21%;
-    padding-left: 21%;
+    padding: 0 15%;
     margin-right: auto;
     margin-left: auto;
+
+    @media screen and (max-width: 1200px) {
+        padding: 0 10%;
+    }
+
+    @media screen and (max-width: 745px) {
+        padding: 0 5%;
+    }
 `
