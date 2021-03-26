0302f8d36e66778e6434815b5906ed8b2ae029d7
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-03-27 02:16:20 +0300

    Site header finished

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,21 +1,73 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import '../../styles/burgerMenu.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { slide as Menu } from 'react-burger-menu';
 
-import { NAV_LINKS } from './NavMap';
+import Logo from '../../assets/images/Header/Logo.png';
+import { INFORM_DATA, NAV_DATA } from './HeaderData';
+import { Wrapper } from '../../styles/Wrapper';
+import { Container } from '../../styles/Container';
+import {
+    HeaderWrapper,
+    HeaderLogo,
+    LogoImg,
+    LogoTitle,
+    HeaderBlock,
+    BlockInform,
+    InformItem,
+    BlogNav,
+    NavText,
+    NavMenu,
+    MyNavLink } from '../../styles/Header';
 
 
 export const Header = () => {
     return (
-        <> 
-        {NAV_LINKS.map(({ path, title }) =>
-            <NavLink 
-                key={title} 
-                to={path} 
-                activeStyle={{ background: '#005FA3' }}
-            >
-                <span>{title}</span>
-            </NavLink>
-        )}
-        </>
+        <Wrapper header bgGray>
+            <NavMenu>
+                <Menu right>
+                    {NAV_DATA.map(({ id, path, title }) =>
+                        <MyNavLink 
+                            key={id} 
+                            to={path} 
+                            activeClassName='active'
+                        >
+                            <NavText>{title}</NavText>
+                        </MyNavLink>
+                    )}
+                </Menu>
+            </NavMenu>
+            <Container>
+                <HeaderWrapper>
+                    <HeaderLogo>
+                        <LogoImg>
+                            <img src={Logo} alt="logo" />
+                        </LogoImg>
+                        <LogoTitle>BuhOne</LogoTitle>
+                    </HeaderLogo>
+                    <HeaderBlock>
+                        <BlockInform>
+                            {INFORM_DATA.map(({ id, avatar, title }) =>
+                                <InformItem key={id}>
+                                    <FontAwesomeIcon icon={avatar} size="1x" />
+                                    <span>{title}</span>
+                                </InformItem>
+                            )}
+                        </BlockInform>
+                        <BlogNav>
+                            {NAV_DATA.map(({ id, path, title }) =>
+                                <MyNavLink 
+                                    key={id} 
+                                    to={path} 
+                                    activeClassName='active'
+                                >
+                                    <NavText>{title}</NavText>
+                                </MyNavLink>
+                            )}
+                        </BlogNav>
+                    </HeaderBlock>
+                </HeaderWrapper>
+            </Container>
+        </Wrapper>
     )
 }
