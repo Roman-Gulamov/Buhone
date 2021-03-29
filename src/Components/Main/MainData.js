86c2b46b5b24984297c9d14e00d944ffa55640c0
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-03-29 04:38:28 +0300

    Data options for carousel

---
+++ src/Components/Main/MainData.js
@@ -0,0 +1,28 @@
+import SaintP from '../../assets/images/SaintP.jpg';
+import Hermitage from '../../assets/images/Main/Hermitage.jpg';
+import Canal from '../../assets/images/Main/Canal.jpg';
+import Park from '../../assets/images/Main/Park.jpg';
+
+
+export const MAIN_DATA = [
+    {
+        id: '1',
+        img: SaintP,
+        title: 'Бухгалтерские услуги в Санкт-Петербурге'
+    },
+    {
+        id: '2',
+        img: Hermitage,
+        title: 'Быстрая и качественная работа'
+    },
+    {
+        id: '3',
+        img: Canal,
+        title: 'Правильный подход к любой компании'
+    },
+    {
+        id: '4',
+        img: Park,
+        title: 'Профессиональная помощь в любых вопросах'
+    }
+]
