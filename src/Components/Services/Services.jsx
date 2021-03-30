2ee46687ce2fc67c599511e0531b1d3a7dc964f7
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-03-30 04:30:00 +0300

    Services component partially completed

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
+import { SERVICES_DATA } from './ServicesData';
 import { Container } from '../../styles/Container';
+import { Wrapper } from '../../styles/Wrapper';
+import {
+    ServicesDescription,
+    ServicesTitle,
+    ServicesContent,
+    ContentItem,
+    ItemImg,
+    ItemBlack,
+    BlackInner,
+    ItemText,
+    ItemHover } from '../../styles/Services';
+
 
 export const Services = () => {
     return (
-        <Container>
-            Services
-        </Container>
+        <>
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>BuhOne | Услуги</title>
+            <meta name="description" content="Profile" />
+            <link rel="canonical" href="https://roman-gulamov.github.io/Buhone/#/services" />
+        </Helmet>
+        <Wrapper bgGray>
+            <Container>
+                <ServicesDescription>Наши услуги</ServicesDescription>
+                <ServicesTitle>Мы специализируемся</ServicesTitle>
+                <ServicesContent>
+                    {SERVICES_DATA.map(({ id, img, title }) =>
+                        <ContentItem key={id}>
+                            <ItemImg>
+                                <img src={img} alt={title} />
+                            </ItemImg>
+                            <ItemBlack>
+                                <BlackInner>
+                                    <ItemText>
+                                        {title}
+                                    </ItemText>
+                                </BlackInner>
+                            </ItemBlack>
+                            <ItemHover></ItemHover>
+                        </ContentItem>
+                    )}
+                </ServicesContent>
+            </Container>
+        </Wrapper>
+        </>
     )
 }
