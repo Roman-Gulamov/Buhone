6300fed38c31a65b0662e46c69e7e5bed3ef8c8e
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-03-30 04:28:33 +0300

    Data parameters for the Services component

---
+++ src/Components/Services/ServicesData.js
@@ -0,0 +1,40 @@
+import Skyscraper from '../../assets/images/Services/Skyscraper.jpg';
+import BurjKhalifa from '../../assets/images/Services/BurjKhalifa.jpg';
+import Bottle from '../../assets/images/Services/Bottle.jpg';
+import TwinTowers from '../../assets/images/Services/TwinTowers.jpg';
+import Sky from '../../assets/images/Services/Sky.jpg';
+import Crescent from '../../assets/images/Services/Crescent.jpg';
+
+
+export const SERVICES_DATA = [
+    {
+        id: '1',
+        img: Skyscraper,
+        title: 'Бухгалтерское обслуживание'
+    },
+    {
+        id: '2',
+        img: BurjKhalifa,
+        title: 'Работа с бумагами'
+    },
+    {
+        id: '3',
+        img: Bottle,
+        title: 'Холодные звонки'
+    },
+    {
+        id: '4',
+        img: TwinTowers,
+        title: 'Консультирование'
+    },
+    {
+        id: '5',
+        img: Sky,
+        title: 'Заключение договоров'
+    },
+    {
+        id: '6',
+        img: Crescent,
+        title: 'Исполнение актов'
+    }
+]
