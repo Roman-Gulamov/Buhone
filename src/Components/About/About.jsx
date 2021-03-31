7cf31724a37d6b9f99d4e95ff30db4a6969d4946
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-03-31 04:46:35 +0300

    The About component is almost complete

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+import { ABOUT_DATA } from './AboutData';
+
+import { Wrapper, Description, Title } from '../../styles/Reusable';
+import { Container } from '../../styles/Container';
+import {
+    AboutWrapper,
+    AboutIntroduce,
+    AboutText,
+    AboutContent,
+    ContentItem,
+    ItemCount,
+    ItemText,
+    MyLink
+} from '../../styles/About';
+
 
 export const About = () => {
     return (
-        <div>
-            About
-        </div>
+        <>
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>BuhOne | Кейсы</title>
+            <meta name="description" content="About" />
+            <link rel="canonical" href="https://roman-gulamov.github.io/Buhone/#/about" />
+        </Helmet>
+        <Wrapper bgWhite>
+            <Container>
+                <AboutWrapper>
+                    <AboutIntroduce>
+                        <Description>О нас</Description>
+                        <Title about={+true}>
+                            Компания
+                            <span>ИвановПром</span>
+                        </Title>
+                        <AboutText>
+                            Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.
+                            Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. 
+                            В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов.
+                            Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн.
+                        </AboutText>
+                    </AboutIntroduce>
+                    <AboutContent>
+                        {ABOUT_DATA.map(({ id, count, text }) =>
+                            <ContentItem key={id}>
+                                <ItemCount>{count}</ItemCount>
+                                <ItemText>{text}</ItemText>
+                                <MyLink to='/contact'>Все кейсы</MyLink>
+                            </ContentItem>
+                        )}
+                    </AboutContent>
+                </AboutWrapper>
+            </Container>
+        </Wrapper>
+        </>
     )
 }
