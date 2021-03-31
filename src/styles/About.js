2cef5478757ae0140ed01bd06832f0d8bb825857
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-03-31 04:46:16 +0300

    Styles for the About block

--- src/styles/About.js
+++ src/styles/About.js
@@ -1,1 +1,56 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const AboutWrapper = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+`
+
+export const AboutIntroduce = styled.div`
+    width: 45%;
+
+    @media screen and (max-width: 621px) {
+        width: 100%;
+    }
+`
+
+export const AboutText = styled.p`
+    font-weight: 300;
+    color: ${props => props.theme.colors.textColor};
+`
+
+export const AboutContent = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+    width: 45%;
+    padding-top: 1em;
+
+    @media screen and (max-width: 621px) {
+        width: 100%;
+    }
+`
+
+export const ContentItem = styled.div`
+    width: 50%;
+    background-color: ${props => props.theme.colors.bgGray};
+
+    @media screen and (max-width: 621px) {
+        width: 100%;
+    }
+`
+
+export const ItemCount = styled.div`
+    font-size: 3em;
+    font-weight: 900;
+`
+
+export const ItemText = styled.div`
+    font-weight: 700;
+`
+
+export const MyLink = styled(Link)`
+    color: ${props => props.theme.colors.brandColor};
+    font-weight: 700;
+    text-decoration: underline;
+`
