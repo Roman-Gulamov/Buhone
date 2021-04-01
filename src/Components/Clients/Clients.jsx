580737a544427f809a2a05b9d567b6236ef74001
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-04-01 04:15:31 +0300

    Client component is complete

--- src/Components/Clients/Clients.jsx
+++ src/Components/Clients/Clients.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+import { CLIENTS_DATA } from './ClientsData';
+import { carouselSettings } from '../Main/carouselSettings';
+
+import { Wrapper, Description, Title } from '../../styles/Reusable';
+import { Container } from '../../styles/Container';
+import { MyCarousel, CarouselImg } from '../../styles/Clients';
+
 
 export const Clients = () => {
     return (
-        <div>
-            Clients
-        </div>
+        <>
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>BuhOne | Клиенты</title>
+            <meta name="description" content="Clients" />
+            <link rel="canonical" href="https://roman-gulamov.github.io/Buhone/#/clients" />
+        </Helmet>
+        <Wrapper bgGray clients>
+            <Container>
+                <Description about={+true}>Наши клиенты</Description>
+                <Title clients>С нами работают</Title>
+                <MyCarousel {...carouselSettings}>
+                    {CLIENTS_DATA.map(({ id, brandOne, altOne, brandTwo, altTwo, brandThree, altThree, brandFour, altFour }) =>
+                        <CarouselImg key={id}>
+                            <img src={brandOne} alt={altOne} />
+                            <img src={brandTwo} alt={altTwo} />
+                            <img src={brandThree} alt={altThree} />
+                            <img src={brandFour} alt={altFour} />
+                        </CarouselImg>
+                    )}
+                </MyCarousel>
+            </Container>
+        </Wrapper>
+        </>
     )
 }
