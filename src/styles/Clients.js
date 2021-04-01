cb1fced6d129d689cc4e00a2083fbc3472e29ced
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-04-01 04:14:05 +0300

    Styles for the Services component

--- src/styles/Clients.js
+++ src/styles/Clients.js
@@ -1,1 +1,86 @@
 import styled from 'styled-components';
+import { Carousel } from 'react-responsive-carousel';
+
+
+export const CarouselImg = styled.div`
+    text-align: left;
+
+    &&& img {
+        width: 24%;
+        height: auto;
+    }
+`
+
+export const MyCarousel = styled(Carousel)`
+    &&& .carousel-slider {
+        overflow: visible;
+    }
+
+    &&& .slider-wrapper {
+        padding-bottom: 6em;
+    }
+
+    &&& .control-dots {
+        display: flex;
+
+        @media screen and (max-width: 1024px) {
+            justify-content: center;
+        }
+
+        & .dot {
+            background-color: ${props => props.theme.colors.brandColor};
+            margin-right: 2em;
+            opacity: .4;
+
+            &.selected {
+                opacity: 1;
+            }
+        }
+    }
+
+    &&& .control-arrow {
+        height: 2em;
+        width: 2em;
+        background: ${props => props.theme.colors.brandColor};
+        opacity: 1;
+        transition: all .4s ease;
+
+        @media screen and (max-width: 1275px) {
+            display: none;
+        }
+
+        &:active, &:hover {
+            opacity: .7;
+        }
+    }
+    
+    &&& .control-prev {
+        left: -4%;
+        background: ${props => props.theme.colors.brandColor};
+
+        @media screen and (max-width: 1550px) {
+            left: -6%;
+        }
+
+        &:before {
+            content: '<';
+            border: none;
+            color: white;
+        }
+    }
+
+    &&& .control-next {
+        right: -4%;
+        background: ${props => props.theme.colors.brandColor};
+
+        @media screen and (max-width: 1550px) {
+            right: -6%;
+        }
+
+        &:before {
+            content: '>';
+            border: none;
+            color: white;
+        }
+    }
+`
