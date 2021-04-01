a3f1e5ec3ec396bc81bc4db9d91b3fcae93cb6e7
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-04-01 04:12:02 +0300

    Images for the Services component

---
+++ src/Components/Clients/ClientsData.js
@@ -0,0 +1,54 @@
+import Microsoft from '../../assets/images/Clients/Microsoft.png';
+import Amazon from '../../assets/images/Clients/Amazon.png';
+import GitHub from '../../assets/images/Clients/GitHub.png';
+import Android from '../../assets/images/Clients/Android.png';
+import Twitter from '../../assets/images/Clients/Twitter.png';
+import YouTube from '../../assets/images/Clients/YouTube.png';
+
+
+export const CLIENTS_DATA = [
+    {
+        id: '1',
+        brandOne: Microsoft,
+        altOne: 'Microsoft',
+        brandTwo: Amazon,
+        altTwo: 'Amazon',
+        brandThree: Twitter,
+        altThree: 'Twitter',
+        brandFour: GitHub,
+        altFour: 'GitHub'
+    },
+    {
+        id: '2',
+        brandOne: Android,
+        altOne: 'Android',
+        brandTwo: YouTube,
+        altTwo: 'YouTube',
+        brandThree: Twitter,
+        altThree: 'Twitter',
+        brandFour: Microsoft,
+        altFour: 'Microsoft'
+    },
+    {
+        id: '3',
+        brandOne: YouTube,
+        altOne: 'YouTube',
+        brandTwo: Microsoft,
+        altTwo: 'Microsoft',
+        brandThree: Amazon,
+        altThree: 'Amazon',
+        brandFour: Android,
+        altFour: 'Android'
+    },
+    {
+        id: '4',
+        brandOne: GitHub,
+        altOne: 'GitHub',
+        brandTwo: Microsoft,
+        altTwo: 'Microsoft',
+        brandThree: Twitter,
+        altThree: 'Twitter',
+        brandFour: YouTube,
+        altFour: 'YouTube'
+    }
+]
