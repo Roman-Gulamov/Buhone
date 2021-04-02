968d770a9a0f926bfa488f71009300da3427c7b5
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-04-02 03:43:06 +0300

    Data file for component Reviews

---
+++ src/Components/Reviews/ReviewsData.js
@@ -0,0 +1,37 @@
+import Catherine from '../../assets/images/Reviews/Catherine.jpg';
+import Ivan from '../../assets/images/Reviews/Ivan.png';
+import Petr from '../../assets/images/Reviews/Petr.png';
+import Julia from '../../assets/images/Reviews/Julia.png';
+
+
+export const REVIEWS_DATA = [
+    {
+        id: '1',
+        photo: Catherine,
+        name: 'екатерина иванова',
+        position: 'директор ООО "Раз-два"', //!
+        review: 'Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил, что он у себя в постели превратился в страшное насекомое. Лежа на панцирнотвердой спине, он видел, стоило ему приподнять голову, свой коричневый, выпуклый, разделенный дугообразными чешуйками живот, на верхушке которого еле держалось готовое вот-вот окончательно сползти одеяло.'
+    },
+    {
+        id: '2',
+        photo: Ivan,
+        name: 'Иван Иванов',
+        position: 'председатель ООО "Три-четыре"', //!
+        review: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sediam nonumy eirmod tempor invidunt ut labore et dolore aliquyam erat, sed diam voluptua. At vero eos et accusam et justo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor et dolore aliquyam erat. Lorem ipsum dolor sit amet.'
+    },
+    {
+        id: '3',
+        photo: Petr,
+        name: 'Пётр Смирнов',
+        position: 'генеральный директор ООО "Пять-шесть"', //!
+        review: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Вдали от всех живут они в буквенных домах на берегу Семантика большого языкового океана. Маленький ручеек Даль журчит по всей стране и обеспечивает ее всеми необходимыми правилами. Эта парадигматическая страна, в которой жаренные члены предложения залетают прямо в рот. '
+    },
+    {
+        id: '4',
+        photo: Julia,
+        name: 'Юлия Гаврилина',
+        position: 'владелец ООО "Семь-восемь"', //!
+        review: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sediam nonumy eirmod tempor invidunt ut labore et dolore aliquyam erat, sed diam voluptua. At vero eos et accusam et justo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor et dolore aliquyam erat. Lorem ipsum dolor sit amet. Lorem ipsum eat.'
+    },
+
+]
