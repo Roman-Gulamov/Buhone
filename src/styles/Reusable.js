61b3cedd3e6d155e1fc6aa5eb6f25551a2f1a23e
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-04-06 03:51:45 +0300

    Added styles for the Title component

--- src/styles/Reusable.js
+++ src/styles/Reusable.js
@@ -4,12 +4,13 @@ import styled, { css } from 'styled-components';
 export const Wrapper = styled.div`
     ${props => props.header && css`
         height: 17vh;
+        z-index: 9999;
 
         @media screen and (max-width: 1024px) {
             margin-bottom: 2em;
         }
 
-        @media screen and (max-width: 281px) {
+        @media screen and (max-width: 321px) {
             margin-bottom: 4em;
         }
     `}
@@ -20,6 +21,28 @@ export const Wrapper = styled.div`
 
         @media screen and (max-width: 600px) {
             height: 79vh;
+        }
+    `}
+    
+    ${props => props.contact && css`
+        position: relative;
+        height: 83vh;
+        padding-top: 4em;
+        overflow: hidden;
+
+        @media screen and (max-width: 600px) {
+            height: 79vh;
+        }
+
+        &::after {
+            content: "";
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.7);
+            z-index: -1;
         }
     `}
 
@@ -86,6 +109,10 @@ export const Title = styled.h1`
             font-family: inherit;
             color: ${props => props.theme.colors.brandColor};
         }
+    `}
+
+    ${props => props.contact && css`
+        color: #fff;
     `}
 
     ${props => props.carousel && css`
