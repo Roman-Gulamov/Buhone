39607405bdf68b8bec302cb6ea830cd32965f96c
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-04-09 06:19:18 +0300

    Updated schema

--- src/Components/Contact/LoginSchema.js
+++ src/Components/Contact/LoginSchema.js
@@ -1,14 +1,13 @@
 import * as Yup from "yup";
 
-
 export const LoginSchema = Yup.object().shape({
-    username: Yup.string()
-        .matches(/^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/u, "Please enter your real name")
-        .required("Please enter your name"),
-    email: Yup.string()
-        .matches(/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/, "Please enter a valid email address")
-        .required("Please enter your email"),
+    name: Yup.string()
+        .matches(/^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/u, "Введите ваше настоящее имя")
+        .required("Введите ваше имя"),
+    surname: Yup.string()
+        .matches(/^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/u, "Введите вашу настояющую фамилию")
+        .required("Введите вашу фамилию"),
     message: Yup.string()
-        .min(5, "Please enter a message more than 5 letters")
-        .required("Please enter your message")
+        .min(5, "Пожалуйста, введите сообщение более 5 букв")
+        .required("Введите сообщение")
 });
