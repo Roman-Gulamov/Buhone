dd5b294ac29169309928d7ee31e3c7525bba4875
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-04-11 18:45:41 +0300

    Updated function

--- src/Components/Contact/goHome.js
+++ src/Components/Contact/goHome.js
@@ -3,8 +3,11 @@ import { createBrowserHistory } from "history";
 export const goHome = () => {
     const history = createBrowserHistory();
 
-    history.push('/Buhone/#/main');
-    history.go(0);
+    setTimeout(() => {
+        history.push('/Buhone/#/main');
+        history.go(0);
+    
+        window.location.reload(); // for mobile phone
+    }, 2000);
 
-    window.location.reload(); // for mobile phone
 }
